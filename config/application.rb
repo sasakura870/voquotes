b6ccc8f12347535d81ee049fbb2e527e4aecc22d
sasakura_870 <a_20100409@yahoo.co.jp>
2020-02-18 17:40:21 +0900

    update rspecの設定の変更
    余分なテストファイルが作成されないように修正

--- config/application.rb
+++ config/application.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require_relative 'boot'
 
 require 'rails/all'
@@ -15,5 +17,20 @@ module Voquotes
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # RSpecの設定
+    config.generators do |g|
+      g.helper false
+      g.test_framework :rspec,
+                       fixtures: true,
+                       view_specs: false,
+                       helper_specs: false,
+                       routing_specs: false,
+                       controller_specs: false,
+                       request_specs: true
+      g.fixture_replacement :factory_bot, dir: 'spec/factories'
+
+      g.assets false
+    end
   end
 end
