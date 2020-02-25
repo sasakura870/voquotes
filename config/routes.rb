21730b4af16967389928fad24e73125c6d8e215b
sasakura_870 <a_20100409@yahoo.co.jp>
2020-02-25 16:15:03 +0900

    update rootページの変更

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   resources :celebrities, only: %i[index show]
   resources :categories, only: %i[index show]
 
-  root 'celebrities#index'
+  root 'quotes#index'
 end
