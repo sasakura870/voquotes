d0319f6c51586d4bdaef2ff30509896526ad4736
sasakura_870 <a_20100409@yahoo.co.jp>
2020-02-26 09:30:17 +0900

    update Migrationの実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_25_084428) do
+ActiveRecord::Schema.define(version: 2020_02_26_002300) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,8 +38,8 @@ ActiveRecord::Schema.define(version: 2020_02_25_084428) do
   end
 
   create_table "quotes", force: :cascade do |t|
-    t.string "content"
-    t.integer "value"
+    t.string "content", null: false
+    t.integer "value", default: 0, null: false
     t.bigint "celebrity_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
