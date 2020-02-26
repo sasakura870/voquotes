ddea9fe278d3b6b6e47fc95845832bead4352333
sasakura_870 <a_20100409@yahoo.co.jp>
2020-02-26 17:29:10 +0900

    update Category, Quoteをhas_many throughで値をとってこれるように修正

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -2,6 +2,7 @@ class Quote < ApplicationRecord
   belongs_to :celebrity
 
   has_many :category_maps, dependent: :destroy
+  has_many :categories, through: :category_maps
 
   validates :content, presence: true
   validates :value, presence: true,
