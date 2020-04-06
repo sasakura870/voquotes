6a4cfb44bfdd11ffba251c98e5a6dd18504728c5
sasakura_870 <a_20100409@yahoo.co.jp>
2020-04-06 22:26:08 +0900

    update pixabayへのapiアクセスを実装

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,14 +1,12 @@
 class QuotesController < ApplicationController
   def index
+    @result = Pixabay.get_pictures query: 'city'
     @quotes = Quote.limit 10
   end
 
-  def show
-  end
+  def show; end
 
-  def new
-  end
+  def new; end
 
-  def create
-  end
+  def create; end
 end
--- lib/pixabay.rb
+++ lib/pixabay.rb
@@ -1,8 +1,14 @@
-class WikipediaApi
+class Pixabay
   class << self
-    def get_request(query:)
+    def get_pictures(query:)
       uri = parse_api_url query: query
+      result = get_request uri: uri
+      result
+    end
 
+    private
+
+    def get_request(uri:)
       https = Net::HTTP.new uri.host, uri.port
       https.use_ssl = true
       response = https.start do |connection|
@@ -10,6 +16,7 @@ class WikipediaApi
         connection.read_timeout = 10
         connection.get uri
       end
+      p uri
 
       begin
         case response
@@ -17,6 +24,8 @@ class WikipediaApi
           result = JSON.parse response.body
         when Net::HTTPRedirection
           result = response
+          # redirect_uri = URI.parse response['location']
+          # result = get_request uri: redirect_uri
         else
           result = { message: "HTTP ERROR: code=#{response.code} message=#{response.message}" }
         end
@@ -30,11 +39,9 @@ class WikipediaApi
       result
     end
 
-    private
-
     def parse_api_url(query:)
       key = Rails.application.credentials.pixabay[:api_key]
-      api_url = 'https://pixabay.com/api'
+      api_url = 'https://pixabay.com/api/'
       params = URI.encode_www_form(
         key: key,
         q: query
